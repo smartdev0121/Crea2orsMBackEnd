421188fa9acbd9d09f2572e83bb05353cf9ea1e2
egan918 <lo.egan918@gmail.com>
2022-08-07 01:03:05 +0900

    dded

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -33,9 +33,9 @@ const upload = multer({
 // 2
 const router = Router();
 
-router.post("/cr2_apis/users", Controllers.User.create);
-router.post("/cr2_apis/auth/login", Controllers.Auth.login);
-router.post("/cr2_apis/auth/forgot_password", Controllers.Auth.forgotPassword);
+// router.post("/cr2_apis/users", Controllers.User.create);
+// router.post("/cr2_apis/auth/login", Controllers.Auth.login);
+// router.post("/cr2_apis/auth/forgot_password", Controllers.Auth.forgotPassword);
 
 router.post("/cr2_apis/search", Controllers.Contract.getSearchAsset);
 //===========================admin router===============================//
@@ -56,8 +56,8 @@ router.post("/cr2_apis/wallet-connected", Controllers.User.setWalletAddress);
 
 router.use(jwt);
 //===================== users profile pages ====================//
-router.post("/cr2_apis/follow", Controllers.Follow.insert);
-router.post("/cr2_apis/unfollow", Controllers.Follow.delete);
+// router.post("/cr2_apis/follow", Controllers.Follow.insert);
+// router.post("/cr2_apis/unfollow", Controllers.Follow.delete);
 router.get("/cr2_apis/custom/:custom_url", Controllers.User.goProfilePage);
 router.get("/cr2_apis/email-verify/:email", Controllers.User.emailVerify);
 router.get(
@@ -92,7 +92,7 @@ router.post("/cr2_apis/cancel-order", Controllers.Contract.cancelOrder);
 router.post("/cr2_apis/nft-minted", Controllers.Contract.nftMinted);
 router.use("/pusher/auth", Controllers.Pusher.auth);
 
-router.post("/cr2_apis/auth/reset_password", Controllers.Auth.resetPassword);
+// router.post("/cr2_apis/auth/reset_password", Controllers.Auth.resetPassword);
 router.get("/cr2_apis/get-user-info", Controllers.User.getUserInfo);
 router.post("/cr2_apis/reportpage", Controllers.Profile.reportPage);
 router.post(
